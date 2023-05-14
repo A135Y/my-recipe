320fa123bcdc2eb68c273a57120727f0780088a1
Abdalla Yusuf <me@a135y.com>
2023-05-14 18:03:30 +0100

    babel fix

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '^.+\\.css$': 'jest-css-modules-transform',
-    },
-};
