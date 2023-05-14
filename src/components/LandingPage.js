750a6b6fcb2d101d7217dff1af352eeddb76e3d4
Abdalla Yusuf <me@a135y.com>
2023-05-14 17:46:16 +0100

    build.yml

---
+++ src/components/LandingPage.js
@@ -0,0 +1,6 @@
+//this will be the landing page for the app
+//this will be the first page the user sees when they open the app
+//use ant design for the layout
+//use rafce to create the component for the landing page
+
+//this will be the landing page for the app
